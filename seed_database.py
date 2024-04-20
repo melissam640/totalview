2d62a5c031a01c0fad889f417e2291eab7fbe064
Melissa McGrath <melissam640@gmail.com>
2024-04-20 11:13:38 -0700

    Added more info to test data in seed_database.py to test edit pages

--- seed_database.py
+++ seed_database.py
@@ -21,32 +21,32 @@ model.db.session.add(user)
 
 # One-time set time event
 event1 = crud.create_event("Event 1", False, "2024-04-15T14:30:00", 
-                           "2024-04-15T15:30:00", "", "", "/delete", "auto",
+                           "2024-04-15T15:30:00", "April 15th 2024 2:30PM", "April 15th 2024 3:30PM", "/edit/1", "auto",
                            "green", "green", "black", None, None, False, user)
 model.db.session.add(event1)
 
 # One-time all day event
-event2 = crud.create_event("Event 2", True, "2024-04-16", "2024-04-16", "", "",
-                           "/delete", "auto", "green", "green", "white", None,
+event2 = crud.create_event("Event 2", True, "2024-04-16", "2024-04-16", "April 16th 2024", "April 16th 2024",
+                           "/edit/2", "auto", "green", "green", "white", None,
                            None, False, user)
 model.db.session.add(event2)
 
 # Repeating daily set time event
 event3 = crud.create_recur_event("Recur Event 3", False, "14:30:00", "15:30:00",
-                          "", "", "/delete", "auto", "green", "green", "black",
+                          "April 17th 2024 2:30PM", "April 20th 2024 3:30PM", "/edit-recur-event/1", "auto", "green", "green", "black",
                           None, "2024-04-17", "2024-04-20", None, None, False,
                           user)
 model.db.session.add(event3)
 
 # Repeating weekly set time event
 event4 = crud.create_recur_event("Recur Event 4", False, "14:30:00", "15:30:00",
-                          "", "", "/delete", "auto", "green", "green", "black",
+                          "April 23rd 2024 2:30PM", "May 3rd 2024 3:30PM", "/edit-recur-event/2", "auto", "green", "green", "black",
                           "2 4", "2024-04-23", "2024-05-03", None, None, False,
                           user)
 model.db.session.add(event4)
 
 # Routine
-routine = crud.create_routine("Routine", "", "", "", "", "/delete", "auto",
+routine = crud.create_routine("Routine", "14:30:00", "15:30:00", "April 9th 2024 2:30PM", "April 12th 2024 3:30PM", "/edit-routine/1", "auto",
                               "red", "red", "white", "2 4", "2024-04-09",
                               "2024-04-12", None, None, False, user)
 model.db.session.add(routine)
